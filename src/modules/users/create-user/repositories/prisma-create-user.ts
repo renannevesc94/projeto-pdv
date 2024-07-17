946f969430c0de42f89531e208e520b8a2694f1b
Renan Neves <renan.epd@gmail.com>
2024-07-16 23:47:46 -0300

    feat: cria custom exception e filter para tratar e retornar erros relacionados ao banco de dados

--- src/modules/users/create-user/repositories/prisma-create-user.ts
+++ src/modules/users/create-user/repositories/prisma-create-user.ts
@@ -2,29 +2,25 @@ import { PrismaService } from 'src/common/prisma/prisma.service';
 import { ICreateUserRepository } from './interface-create-user.repository';
 import { Roles, User } from '../../user.entity';
 import { CreateUserDto } from '../dto/create-user-dto';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PrismaCreateuserRepository implements ICreateUserRepository {
   constructor(private prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    try {
-      const createdUser = await this.prisma.user.create({
-        data: {
-          ...createUserDto,
-        },
-      });
+    const createdUser = await this.prisma.user.create({
+      data: {
+        ...createUserDto,
+      },
+    });
 
-      return new User(
-        createdUser.id,
-        createdUser.name,
-        createdUser.email,
-        createdUser.password,
-        createdUser.role as Roles,
-      );
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.FORBIDDEN);
-    }
+    return new User(
+      createdUser.id,
+      createdUser.name,
+      createdUser.email,
+      createdUser.password,
+      createdUser.role as Roles,
+    );
   }
 }
