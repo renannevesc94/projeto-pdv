8503161955551ea7c51efb7567c5950941f1b491
Renan Neves <renan.epd@gmail.com>
2024-07-19 18:39:21 -0300

    test: cria o teste unitári do serviço DeleteUserService

--- src/modules/users/delete-user/tests/delete-user.service.spec.ts
+++ src/modules/users/delete-user/tests/delete-user.service.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DeleteUserService } from './delete-user.service';
+import { DeleteUserService } from '../delete-user.service';
+import { IDeleteUserRepository } from '../repositories/interface-delete-user.repository';
 
 describe('DeleteUserService', () => {
   let service: DeleteUserService;
+  let deleteUserRepository: IDeleteUserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DeleteUserService],
+      providers: [
+        DeleteUserService,
+        {
+          provide: IDeleteUserRepository,
+          useValue: {
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<DeleteUserService>(DeleteUserService);
+    deleteUserRepository = module.get<IDeleteUserRepository>(
+      IDeleteUserRepository,
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should call the delete method of deleteUserRepository', async () => {
+    const email = 'test@example.com';
+    await service.delete(email);
+    expect(deleteUserRepository.delete).toHaveBeenCalledWith(email);
   });
 });
