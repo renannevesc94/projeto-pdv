32fc00d27ada1b5c8bec240e38c55a767d325144
Renan Neves <renan.epd@gmail.com>
2024-08-02 18:41:09 -0300

    feat: cria seed para popular banco de dados para realizar os testes e2e

---
+++ prisma/seedTestDatabase.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client';
+import { Roles } from 'src/modules/users/user.entity';
+
+export async function seedTestDatabase() {
+  const prisma = new PrismaClient();
+  const users = [
+    {
+      name: 'Admin',
+      email: 'admin@projetopdv.com',
+      password: '12345678',
+      role: 'ADMINISTRADOR',
+    },
+
+    {
+      name: 'Supervisor',
+      email: 'supervisor@projetopdv.com',
+      password: '12345678',
+      role: 'SUPERVISOR',
+    },
+
+    {
+      name: 'Operador',
+      email: 'operador@projetopdv.com',
+      password: '12345678',
+      role: 'OPERADOR',
+    },
+  ];
+
+  await prisma.user.deleteMany();
+  for (const user of users) {
+    await prisma.user.upsert({
+      where: { email: user.email },
+      update: {
+        name: user.name,
+        password: user.password,
+        role: user.role as Roles,
+      },
+      create: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        role: user.role as Roles,
+      },
+    });
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-
 import { CommomModule } from './common/commom.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [CommomModule, UsersModule],
-
   controllers: [],
 })
 export class AppModule {}
