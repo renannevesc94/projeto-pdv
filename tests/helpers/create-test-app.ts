9f73906029bc28e4f727601fed703a381e9b620c
Renan Neves <renan.epd@gmail.com>
2024-08-02 18:38:53 -0300

    feat: cria um helper para configurar e inicializar um app jest para os testes

--- tests/helpers/create-test-app.ts
+++ tests/helpers/create-test-app.ts
@@ -1,20 +1,34 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { seedTestDatabase } from 'prisma/seedTestDatabase';
 import { AppModule } from 'src/app.module';
 import { PrismaInitiazilationExceptionFilter } from 'src/common/filters/prisma-initiazilation-exception.filter';
 import { PrismaRequestExceptionFilter } from 'src/common/filters/prisma-request-exception.filter';
+
+let app: INestApplication;
 
 export const createTestApp = async (): Promise<INestApplication> => {
   const moduleFixture: TestingModule = await Test.createTestingModule({
     imports: [AppModule],
   }).compile();
 
-  const app = moduleFixture.createNestApplication();
-  app.useGlobalFilters(
+  const appInstance = moduleFixture.createNestApplication();
+  appInstance.useGlobalFilters(
     new PrismaRequestExceptionFilter(),
     new PrismaInitiazilationExceptionFilter(),
   );
 
-  await app.init();
-  return app;
+  await appInstance.init();
+  return appInstance;
 };
+
+beforeAll(async () => {
+  app = await createTestApp();
+  await seedTestDatabase();
+});
+
+afterAll(async () => {
+  await app.close();
+});
+
+export { app };
