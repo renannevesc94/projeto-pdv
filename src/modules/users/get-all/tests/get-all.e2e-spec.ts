41fea1ff01cfe0c9c16e8ccff01864dca9c5ea9e
Renan Neves <renan.epd@gmail.com>
2024-08-02 18:36:30 -0300

    test: implementa teste e2e na rota de busca de todos usuários

--- src/modules/users/get-all/tests/get-all.e2e-spec.ts
+++ src/modules/users/get-all/tests/get-all.e2e-spec.ts
@@ -1,13 +1,7 @@
-import { INestApplication } from '@nestjs/common';
-import { createTestApp } from '../../../../../tests/helpers/create-test-app';
 import * as request from 'supertest';
+import { app } from 'tests/helpers/create-test-app';
 
 describe('Get All Users Tests (e2e)', () => {
-  let app: INestApplication;
-  beforeEach(async () => {
-    app = await createTestApp();
-  });
-
   it('(GET) should return an array of users', () => {
     return request(app.getHttpServer())
       .get('/users')
