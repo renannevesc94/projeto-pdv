7d8fa0f6087cbae5f3eb409dde5148c0335d7f70
Renan Neves <renan.epd@gmail.com>
2024-08-03 16:41:36 -0300

    feat: implementa o provider do módulo auth no app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommomModule } from './common/commom.module';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [CommomModule, UsersModule],
+  imports: [CommomModule, UsersModule, AuthModule],
   controllers: [],
 })
 export class AppModule {}
