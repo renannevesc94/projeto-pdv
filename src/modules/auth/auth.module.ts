d677208b417b671ae3a25586b958006f4d169cc6
Renan Neves <renan.epd@gmail.com>
2024-08-03 16:30:36 -0300

    feat: cria o módulo para rota de autenticação

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { LoginService } from './login/login.service';
+import { ILoginRepository } from './login/repositories/interface-login.repository';
+import { PrismaLoginRepository } from './login/repositories/prisma-login.repository';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
-@Module({})
+@Module({
+  controllers: [AuthController],
+  providers: [
+    LoginService,
+    PrismaService,
+    {
+      provide: ILoginRepository,
+      useClass: PrismaLoginRepository,
+    },
+  ],
+})
 export class AuthModule {}
