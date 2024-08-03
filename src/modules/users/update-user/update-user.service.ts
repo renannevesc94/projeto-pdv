6c5a58c9d3d4b151348dbad5a608ad4dd51ac038
Renan Neves <renan.epd@gmail.com>
2024-08-03 16:26:32 -0300

    feat: implementa o hash do password ao ser editado

--- src/modules/users/update-user/update-user.service.ts
+++ src/modules/users/update-user/update-user.service.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
 import { IUpdateUserRepository } from './repositories/interface-update-user.repository';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -6,6 +7,11 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UpdateUserService {
   constructor(private readonly updateUserRepository: IUpdateUserRepository) {}
   async update(email: string, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      const hashedPassword = await bcrypt.hash(updateUserDto.password, 10);
+      updateUserDto.password = hashedPassword;
+    }
+
     return await this.updateUserRepository.update(email, updateUserDto);
   }
 }
