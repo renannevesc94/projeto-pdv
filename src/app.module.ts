71268c923744fbaa02e715b1cefa9b74ed9875f9
Renan Neves <renan.epd@gmail.com>
2024-08-06 20:54:56 -0300

    feat: Implementa o import dos modulos para os recursos já comitados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { CommomModule } from './common/commom.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 
 @Module({
   imports: [CommomModule, UsersModule, AuthModule],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: JwtAuthGuard,
+    },
+  ],
   controllers: [],
 })
 export class AppModule {}
--- src/common/commom.module.ts
+++ src/common/commom.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Module({
-  imports: [],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  imports: [
+    JwtModule.register({
+      secret: process.env.SECRET_JWT,
+    }),
+  ],
+  providers: [PrismaService, JwtStrategy, JwtAuthGuard],
+  exports: [PrismaService, JwtAuthGuard],
 })
 export class CommomModule {}
