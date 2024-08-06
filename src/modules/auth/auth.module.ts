396b5ea80e24f0a314af75ec07d0bcc6842c62f1
Renan Neves <renan.epd@gmail.com>
2024-08-06 20:54:09 -0300

    feat: Implementa o JWTModule no import do auth.module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,12 +4,23 @@ import { LoginService } from './login/login.service';
 import { ILoginRepository } from './login/repositories/interface-login.repository';
 import { PrismaLoginRepository } from './login/repositories/prisma-login.repository';
 import { PrismaService } from 'src/common/prisma/prisma.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './login/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.SECRET_JWT,
+    }),
+  ],
   controllers: [AuthController],
   providers: [
+    LocalStrategy,
     LoginService,
     PrismaService,
+
     {
       provide: ILoginRepository,
       useClass: PrismaLoginRepository,
