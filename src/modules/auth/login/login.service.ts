3a383344fd8268b7d4aa127bf1a62c7f1a0006f1
Renan Neves <renan.epd@gmail.com>
2024-08-06 20:44:04 -0300

    feat: Implementa o método authenticateUser  e ajusta o método login para retornar o Token JWT

--- src/modules/auth/login/login.service.ts
+++ src/modules/auth/login/login.service.ts
@@ -1,21 +1,37 @@
-import * as bcrypt from 'bcrypt';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ILoginRepository } from './repositories/interface-login.repository';
-import { AuthCredentialsDto } from './dto/auth-credentials-dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/modules/users/user.entity';
 
 @Injectable()
 export class LoginService {
-  constructor(private readonly loginRepository: ILoginRepository) {}
+  constructor(
+    private readonly loginRepository: ILoginRepository,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async login(authCredentialsDto: AuthCredentialsDto) {
-    const { email, password } = authCredentialsDto;
-    const user = await this.loginRepository.getUser(email);
+  async authenticateUser(email: string, password: string) {
+    const user = await this.loginRepository.getByEmail(email.trim());
+
     if (user) {
       const isMatch = await bcrypt.compare(password, user.password);
       if (isMatch) {
-        return new HttpException('Login successful', HttpStatus.OK);
+        const { password, ...result } = user;
+        return result;
       }
     }
-    throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+
+    return null;
+  }
+
+  async login(user: User) {
+    const payload = { userId: user.id, role: user.role };
+    const jwtToken = this.jwtService.sign(payload, {
+      expiresIn: '1d',
+    });
+
+    return jwtToken;
   }
 }
