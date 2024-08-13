bf13d63a78e17a1fdcee068343cb88afe8a84a5c
Renan Neves <renan.epd@gmail.com>
2024-08-13 13:08:44 -0300

    feat: adciona  o import do CategoriesModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { CommomModule } from './common/commom.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [CommomModule, UsersModule, AuthModule],
+  imports: [CommomModule, UsersModule, AuthModule, CategoriesModule],
   providers: [
     {
       provide: 'APP_GUARD',
