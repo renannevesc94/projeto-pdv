cb2b6023959316caa61e112055df8738e7f17cd3
Renan Neves <renan.epd@gmail.com>
2024-08-26 11:38:47 -0300

    test: Cria test e2e para criação de supplier

---
+++ src/modules/suppliers/create-supplier/tests/create-supplier.e2e-spec.ts
@@ -0,0 +1,52 @@
+import * as request from 'supertest';
+import { app, token } from 'tests/helpers/create-test-app';
+
+describe('Create Supplier Tests (e2e)', () => {
+  it('(POST) should return success and 201 status code when sending correct data in the body', () => {
+    return request(app.getHttpServer())
+      .post('/supplier')
+      .set('Authorization', token)
+      .send({
+        name: 'Supplier Test',
+        description: 'Supplier Test Description',
+      })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body).toEqual({
+          name: 'Supplier Test',
+          description: 'Supplier Test Description',
+          id: expect.any(Number),
+        });
+      });
+  });
+
+  it('(POST) should return error and 400 status code when sending incomplete or incorrect data in the body', () => {
+    return request(app.getHttpServer())
+      .post('/supplier')
+      .set('Authorization', token)
+      .send({
+        name: 'Supplier Test',
+        description: '',
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.message[0]).toEqual(
+          'description must be longer than or equal to 5 characters',
+        );
+      });
+  });
+
+  it('(POST) return error and 409 status code when sending duplicate data in the body', () => {
+    return request(app.getHttpServer())
+      .post('/supplier')
+      .set('Authorization', token)
+      .send({
+        name: 'Supplier Test',
+        description: 'Supplier Test Description',
+      })
+      .expect(409)
+      .expect((res) => {
+        expect(res.body.message).toEqual('name already exists');
+      });
+  });
+});
