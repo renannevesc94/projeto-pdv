08e79a93fbe04b2ea8d33d6e679fb868e61f9c3b
Renan Neves <renan.epd@gmail.com>
2024-09-02 18:40:28 -0300

    feat: Implementa as rotas para os métodos update, patch e delete no módulo supplier

--- src/modules/suppliers/supplier.controller.ts
+++ src/modules/suppliers/supplier.controller.ts
@@ -1,7 +1,10 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  Param,
+  Patch,
   Post,
   UsePipes,
   ValidationPipe,
@@ -10,22 +13,38 @@ import { CreateSupplierService } from './create-supplier/create-supplier.service
 import { CreateSupplierDto } from './create-supplier/dto/create-supplier.dto';
 import { TrimBodyPipe } from 'src/common/utils/trim-body.pipe';
 import { FindAllSuppliersService } from './find-all/find-all-suppliers.service';
+import { DeleteSupplierService } from './delete-supplier/delete-supplier.service';
+import { UpdateSupplierService } from './update-supplier/update-supplier.service';
 
 @Controller('supplier')
 export class SupplierController {
   constructor(
     private readonly createSupplierService: CreateSupplierService,
     private readonly findAllSuppliersService: FindAllSuppliersService,
+    private readonly deleteSupplierService: DeleteSupplierService,
+    private readonly updateSupplierService: UpdateSupplierService,
   ) {}
 
   @Post()
   @UsePipes(new TrimBodyPipe(), new ValidationPipe())
-  create(@Body() createSupplierDto: CreateSupplierDto) {
-    return this.createSupplierService.create(createSupplierDto);
+  async create(@Body() createSupplierDto: CreateSupplierDto) {
+    return await this.createSupplierService.create(createSupplierDto);
   }
 
   @Get()
-  findAll() {
-    return;
+  async findAll() {
+    return await this.findAllSuppliersService.findAll();
+  }
+
+  @Delete(':id')
+  @UsePipes(new ValidationPipe())
+  async delete(@Param('id') id: number) {
+    return await this.deleteSupplierService.delete(+id);
+  }
+
+  @Patch(':id')
+  @UsePipes(new TrimBodyPipe(), new ValidationPipe())
+  async update(@Param('id') id: number, @Body() updateSupplierDto: any) {
+    return await this.updateSupplierService.update(+id, updateSupplierDto);
   }
 }
