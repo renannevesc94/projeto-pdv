8c2bc0fe05dc8fd64b28af3be040448e521c06ca
Renan Neves <renan.epd@gmail.com>
2024-09-02 18:39:29 -0300

    feat: Adiciona funcionalidade findAll para listar suppliers cadastradas

---
+++ src/modules/suppliers/find-all/repositories/prisma.find-all-suppliers.repository.ts
@@ -0,0 +1,15 @@
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { IFindAllSuppliersRepository } from './interface-find-all-suppliers.repository';
+import { Supplier } from '../../supplier.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PrismaFindAllSuppliersRepository
+  implements IFindAllSuppliersRepository
+{
+  constructor(private prisma: PrismaService) {}
+  async findAll(): Promise<Supplier[]> {
+    const suppliers = await this.prisma.supplier.findMany();
+    return suppliers;
+  }
+}
--- src/modules/suppliers/supplier.module.ts
+++ src/modules/suppliers/supplier.module.ts
@@ -6,13 +6,21 @@ import { ICreateSupplierRepository } from './create-supplier/repositories/interf
 import { PrismaCreateSupplierRepository } from './create-supplier/repositories/prisma-create-supplier.repository';
 import { FindAllSuppliersService } from './find-all/find-all-suppliers.service';
 import { IFindAllSuppliersRepository } from './find-all/repositories/interface-find-all-suppliers.repository';
-import { PrismaFindAllCategoriesRepository } from '../categories/find-all/repositories/prisma-find-all-categories.repository';
+import { DeleteSupplierService } from './delete-supplier/delete-supplier.service';
+import { IDeleteSupplierRepository } from './delete-supplier/repositories/interface-delete-supplier.repository';
+import { PrismaDeleteSupplierRepository } from './delete-supplier/repositories/prisma-delete-supplier.repository';
+import { PrismaFindAllSuppliersRepository } from './find-all/repositories/prisma.find-all-suppliers.repository';
+import { UpdateSupplierService } from './update-supplier/update-supplier.service';
+import { PrismaUpdateSupplierRepository } from './update-supplier/repositories/prisma-update-supplier.repository';
+import { IUpdateSupplierRepository } from './update-supplier/repositories/interface-update-supplier.repository';
 
 @Module({
   controllers: [SupplierController],
   providers: [
     CreateSupplierService,
     FindAllSuppliersService,
+    DeleteSupplierService,
+    UpdateSupplierService,
     PrismaService,
     {
       provide: ICreateSupplierRepository,
@@ -20,7 +28,15 @@ import { PrismaFindAllCategoriesRepository } from '../categories/find-all/reposi
     },
     {
       provide: IFindAllSuppliersRepository,
-      useClass: PrismaFindAllCategoriesRepository,
+      useClass: PrismaFindAllSuppliersRepository,
+    },
+    {
+      provide: IDeleteSupplierRepository,
+      useClass: PrismaDeleteSupplierRepository,
+    },
+    {
+      provide: IUpdateSupplierRepository,
+      useClass: PrismaUpdateSupplierRepository,
     },
   ],
 })
