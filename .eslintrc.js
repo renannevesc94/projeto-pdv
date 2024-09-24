672b6f94c3c115b67e8f700f46bde34de7f9e498
Renan Neves <renan.epd@gmail.com>
2024-09-24 11:42:28 -0300

    chore: Atualiza configurações do prittier e eslint e atualiza dependencias do projeto

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'block-like', next: 'block-like' },
+    ],
   },
 };
