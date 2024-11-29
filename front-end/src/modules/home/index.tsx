cc519967eb593d2638a817f9e3901cf479ee9515
Renan Neves <renan.epd@gmail.com>
2024-11-29 14:42:56 -0300

    feat: Cria o card de produtos e ajusta a exibição na home

--- front-end/src/modules/home/index.tsx
+++ front-end/src/modules/home/index.tsx
@@ -2,6 +2,7 @@ import { HomeHeader } from "./components/HomeHeader";
 import { Checkout } from "./components/Checkout";
 import { SideBar } from "../../components/Sidebar";
 import styles from "./styles.module.css";
+import { ProductCard } from "../../components/ProductCard";
 
 export const Home = () => {
   return (
@@ -12,6 +13,17 @@ export const Home = () => {
 
       <div className={styles.headerContainer}>
         <HomeHeader />
+      </div>
+
+      <div className={styles.productsCardContainer}>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
 
       <div className={styles.checkoutContainer}>
