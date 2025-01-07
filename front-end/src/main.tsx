ff37c0430b2eb11fdd769ac24ecf6395420e1bf8
Renan Neves <renan.epd@gmail.com>
2025-01-07 17:27:20 -0300

    feat: Implementa alternativa mais eficiente para proteção de rotas

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,51 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Home } from "./modules/home";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./styles/reset.css";
 import "./styles/global.module.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Login } from "./modules/auth";
-import { AuthContextProvider } from "./providers/AuthProvider";
-import { ProtectedRouter } from "./components/ProtectedRouter";
+import { Routes } from "./routes";
+
 const queryClient = new QueryClient();
-
-export const Teste = () => {
-  return <div>AQUI NÂO PODE</div>;
-};
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <AuthContextProvider>
-        <Login />
-      </AuthContextProvider>
-    ),
-  },
-  {
-    element: (
-      <AuthContextProvider>
-        <ProtectedRouter />
-      </AuthContextProvider>
-    ),
-    children: [
-      {
-        path: "/home",
-        element: <Home />,
-      },
-    ],
-  },
-
-  {
-    element: <ProtectedRouter role="ADMINISTRADOR" />,
-    children: [{ path: "/sales", element: <Teste /> }],
-  },
-]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <RouterProvider router={Routes} />
     </QueryClientProvider>
   </StrictMode>
 );
--- front-end/src/modules/auth/hooks/useLogin.ts
+++ front-end/src/modules/auth/hooks/useLogin.ts
@@ -2,10 +2,10 @@ import { useForm } from "react-hook-form";
 import { AuthSchema, AuthType } from "../types/AuthTypes";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { usePostAuth } from "./usePostAuth";
-
-import { useAuth } from "../../../providers/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -14,11 +14,9 @@ export const useLogin = () => {
     resolver: zodResolver(AuthSchema),
   });
 
-  const { login } = useAuth();
-
   const { mutate, error } = usePostAuth({
-    onSuccess: (response) => {
-      login(response.data.role);
+    onSuccess: () => {
+      navigate("/home", { replace: true });
     },
 
     onError: (err) => {
